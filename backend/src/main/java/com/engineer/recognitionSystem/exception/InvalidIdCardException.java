e57f3c7ef8603085d2cb2a4152055bcaa08f3156
Steven <steven88161815@gmail.com>
2024-12-23 00:13:37 +0800

    Revert "feat: tesseract測試"
    This reverts commit d5c92239cc407697a0d3be85b7cf8c225561b3ad.

--- backend/src/main/java/com/engineer/recognitionSystem/exception/InvalidIdCardException.java
+++
@@ -1,8 +0,0 @@
-// InvalidIdCardException.java
-package com.engineer.recognitionSystem.exception;
-
-public class InvalidIdCardException extends RuntimeException {
-    public InvalidIdCardException(String message) {
-        super(message);
-    }
-}
