0fa7ddf840a57b81de85731322997490145b7fa8
Steven <steven88161815@gmail.com>
2024-12-24 22:30:47 +0800

    feat: 內部讀取憑證

--- backend/src/main/java/com/engineer/recognitionSystem/service/OcrService.java
+++ backend/src/main/java/com/engineer/recognitionSystem/service/OcrService.java
@@ -1,15 +1,23 @@
 package com.engineer.recognitionSystem.service;
 
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.vision.v1.*;
 import com.google.protobuf.ByteString;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Collections;
 
 @Service
 public class OcrService {
+
+    @Autowired
+    private ResourceLoader resourceLoader;
 
     public String performOcr(MultipartFile file) throws IOException {
         // 將上傳的圖片轉為 ByteString
@@ -27,15 +35,24 @@ public class OcrService {
                 .setImage(img)
                 .build();
 
-        // 調用 Vision API
-        try (ImageAnnotatorClient vision = ImageAnnotatorClient.create()) {
-            AnnotateImageResponse response = vision.batchAnnotateImages(Collections.singletonList(request))
-                    .getResponses(0);
-            if (response.hasError()) {
-                throw new IOException("Error during OCR: " + response.getError().getMessage());
+        // 讀取憑證
+        Resource resource = resourceLoader.getResource("classpath:google/test.json");
+        try (InputStream is = resource.getInputStream()) {
+            GoogleCredentials credentials = GoogleCredentials.fromStream(is);
+            ImageAnnotatorSettings settings = ImageAnnotatorSettings.newBuilder()
+                    .setCredentialsProvider(() -> credentials)
+                    .build();
+
+            // 調用 Vision API
+            try (ImageAnnotatorClient vision = ImageAnnotatorClient.create(settings)) {
+                AnnotateImageResponse response = vision.batchAnnotateImages(Collections.singletonList(request))
+                        .getResponses(0);
+                if (response.hasError()) {
+                    throw new IOException("Error during OCR: " + response.getError().getMessage());
+                }
+                // 返回檢測的文字
+                return response.getTextAnnotationsList().get(0).getDescription();
             }
-            // 返回檢測的文字
-            return response.getTextAnnotationsList().get(0).getDescription();
         }
     }
 }
